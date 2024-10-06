b005e8b52beec38f2f08a715212697de6c025010
Amay Gada <61084829+amaygada@users.noreply.github.com>
2024-10-06 05:12:45 +0000

    task3 done

--- hw4/task3.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gawk -F, '$3 == 2 && $13 ~/^S/ {print $0}' titanic.csv | sed 's/\<male\>/M/g; s/\<female\>/F/g'
--- task3.sh
+++ task3.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
- gawk '$3 == 2 
+cd ./hw4/
+
+# part a
+# gawk -F, '$3==2 && $13 ~ /^S/ {print $0}' titanic.csv
+
+# part b
+# sed 's/female/F/g; s/male/M/g'
+
+# part c
+# gawk -F, '{if($7 != "") {sum+=$7;count+=1}} END  {print sum/count} '
+
+
+# Output
+# a | b | c
+
+gawk -F, '$3==2 && $13 ~ /^S/ {print $0}' titanic.csv | sed 's/female/F/g; s/male/M/g' | gawk -F, '{if($7 != "") {sum+=$7;count+=1}} END  {print sum/count}'
