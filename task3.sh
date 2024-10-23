86b950489b6fc75b23142a44d8125c6cc784bf0c
shaziamuckram <158780766+shaziamuckram@users.noreply.github.com>
2024-10-23 01:23:01 +0000

    Fixed task3 to print lines of filtered passengers

--- task3.sh
+++ task3.sh
@@ -2,16 +2,12 @@
 cd ./hw4/
 
 # part a
-# gawk -F, '$3==2 && $13 ~ /^S/ {print $0}' titanic.csv
+# gawk -F, '$3==2 && $13 ~ /^S/ {print $0; if($7 != "") {sum+=$7;count+=1}} END {print "Average Age:", sum/count}' titanic.csv
 
 # part b
 # sed 's/female/F/g; s/male/M/g'
 
-# part c
-# gawk -F, '{if($7 != "") {sum+=$7;count+=1}} END  {print sum/count} '
-
-
 # Output
-# a | b | c
+# a | b 
 
-gawk -F, '$3==2 && $13 ~ /^S/ {print $0}' titanic.csv | sed 's/female/F/g; s/male/M/g' | gawk -F, '{if($7 != "") {sum+=$7;count+=1}} END  {print sum/count}'
+gawk -F, '$3==2 && $13 ~ /^S/ {print $0; if($7 != "") {sum+=$7;count+=1}} END {print "Average Age:", sum/count}' titanic.csv | sed 's/female/F/g; s/male/M/g'
